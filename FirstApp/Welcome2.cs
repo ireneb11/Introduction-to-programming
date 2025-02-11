9737c8eb13dfd680efe47837bf36000a730c6a1f
ireneb11 <irene.borja.u@gmail.com>
2025-02-11 19:17:15 +0100

    Create Welcome2.cs

---
+++ FirstApp/Welcome2.cs
@@ -0,0 +1,13 @@
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the world of Game Developement!");
+            Console.WriteLine("Press any key to start your adventure");
+            Console.ReadKey();
+            Console.WriteLine("Loanding...");
+        }
+    }
+}
