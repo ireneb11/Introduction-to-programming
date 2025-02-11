e2e0d9d8a565739c2a047ed1201cf866d197ed5b
ireneb11 <irene.borja.u@gmail.com>
2025-02-11 08:30:35 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+    
+    namespace PrimeraApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //Console.Readkey is used to wait for user key iput
+            Console.ReadKey();
+        }
+    }
+}
