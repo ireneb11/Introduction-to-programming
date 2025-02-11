fa39f839bd5c337e129ceaafdb869df4245f85e8
ireneb11 <irene.borja.u@gmail.com>
2025-02-11 21:54:18 +0100

    Create Concatenacion_cadena.cs

---
+++ Basic concepts/Concatenacion_cadena.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcatenacionCadenas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Jugador";
+            string mensaje = "Bienvenido, " + nombre + "!";
+
+            Console.WriteLine("Mensaje: " + mensaje);
+        }
+        // Other functions
+    }
+}
