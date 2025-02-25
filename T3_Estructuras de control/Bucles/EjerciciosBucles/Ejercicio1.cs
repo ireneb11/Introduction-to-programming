ec623a4fc43dfd3aec1e7162cdd7e6d14091752d
ireneb11 <irene.borja.u@gmail.com>
2025-02-25 13:05:26 +0100

    Create Ejercicio1.cs

---
+++ T3_Estructuras de control/Bucles/EjerciciosBucles/Ejercicio1.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bucle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool corriendo = true;
+            int tiempo = 200;
+
+            while (corriendo)
+            {
+                tiempo -= 10;
+                Console.WriteLine($"Tiempo restante {tiempo} segundos.");
+
+                if (tiempo <= 0)
+                {
+                    corriendo = false;
+                    Console.WriteLine("Mario deja de correr.");
+                }
+            }
+
+        }
+    }
+}
