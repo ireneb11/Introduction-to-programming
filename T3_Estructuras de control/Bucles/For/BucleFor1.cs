308d57bc7c71095e6b39617a0021799e27a81eb1
ireneb11 <irene.borja.u@gmail.com>
2025-03-04 12:49:35 +0100

    Create BucleFor1.cs

---
+++ T3_Estructuras de control/Bucles/For/BucleFor1.cs
@@ -0,0 +1,22 @@
+//Library inclusion
+using System;
+using System. Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BucleFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int level = 1; level <= 10; level++)
+            {
+                Console.WriteLine($"Nivel {level} de Super Mario");
+            }
+
+        }
+    }
+}
