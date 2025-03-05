f632eaf0c05da9ac0bf9cca5eb13ab08750715b5
ireneb11 <irene.borja.u@gmail.com>
2025-03-05 10:07:43 +0100

    Update and rename Ejercicio2.cs to EjercicioBucleFor2.cs

--- T3_Estructuras de control/Bucles/For/Ejercicios Bucle For/EjercicioBucleFor2.cs
+++ T3_Estructuras de control/Bucles/For/Ejercicios Bucle For/EjercicioBucleFor2.cs
@@ -5,12 +5,14 @@ namespace EjercicioBucleFor2
         static void Main(string[] args)
         {
             Random random = new Random();
-
             float precioObjeto = 50;
+
             for (int nivel = 1; nivel < 10; nivel++)
             {
+                // Genera un numero aleatorio del 0 al 1000 por cada repetición del bucle.
                 int monedasRecolectadas = random.Next(0,1000);
 
+                // Si el numero aleatorio de monedas supera los 100€, entoces Mario puede comprar y se la aplica un descuento del 20%
                 if (monedasRecolectadas > 100)
                 {
                     precioObjeto *= 0.2f;
@@ -18,7 +20,7 @@ namespace EjercicioBucleFor2
                 }
                 else 
                 {
-                    Console.WriteLine("No tienes suficientes monedas para comprar.");
+                    Console.WriteLine($"Monedas recolectadas en el nivel {nivel}: {monedasRecolectadas}. No tienes suficientes monedas para comprar.");
                 }
             }
         }
